359c67b9e9f8750d03e034c899c653617f816925
lafar10 <ayoub_lafar@hotmail.com>
2021-09-06 14:51:56 +0100

    initial commit

--- resources/views/Front/triporteur.blade.php
+++ resources/views/Front/triporteur.blade.php
@@ -36,9 +36,7 @@
                                                                                                                                                                                                                 style="text-align:left;border-radius:0%;"
                                                                                                                                                                                                             @endif >
                                                             <datalist id="datalistOptions">
-                                                                @foreach ($villes as $value)
-                                                                    <option   value="{{$value->ville}}">
-                                                                @endforeach
+
                                                             </datalist>
                                                             @if($errors->has('lieu_ramassage'))
                                                                 <div class="error" style="color:red;">{{ $errors->first('lieu_ramassage') }}</div>
@@ -54,9 +52,9 @@
                                                                                                                                                                                         style="text-align:left;border-radius:0%;"
                                                                                                                                                                                     @endif >
                                                             <datalist id="datalistOptions">
-                                                                @foreach ($villes as $value)
-                                                                    <option   value="{{$value->ville}}">
-                                                                @endforeach
+
+
+
                                                             </datalist>
                                                             @if($errors->has('lieu_depose'))
                                                                 <div class="error" style="color:red;">{{ $errors->first('lieu_depose') }}</div>
